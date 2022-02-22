0cc44f170e73bdb5a6c308d8ee564808dc1639be
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-22 14:15:11 -0600

    build

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@ cd HW1_CB
 sudo apt-get -y update
 sudo apt-get -y install openjdk-17-jdk
 
-sudo wget https://services.gradle.org/distributions/gradle-7.0-bin.zip
-sudo apt-get -y install unzip
-sudo unzip -d gradle-7.0-bin.zip
+sudo snap install gradle
 
 gradle build
 sudo java -jar build/libs/HW1_CB.jar
