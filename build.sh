6b90772587f5f0247b17e06d888ef39e538667cf
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-22 14:16:58 -0600

    build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd HW1_CB
 sudo apt-get -y update
 sudo apt-get -y install openjdk-17-jdk
 
-sudo snap install gradle
+sudo snap install gradle --classic
 
 gradle build
 sudo java -jar build/libs/HW1_CB.jar
