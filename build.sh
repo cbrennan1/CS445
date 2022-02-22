d7563b371e1396a47b4b68f04d7da9570050e733
Colin Brennan <cbrennan1@hawk.iit.edu>
2022-02-22 13:49:01 -0600

    build permissions

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ cd HW1_CB
 sudo apt-get -y update
 sudo apt-get -y install openjdk-17-jdk
 
-./gradlew build
-java -jar build/libs/HW1_CB.jar
+sudo ./gradlew build
+sudo java -jar build/libs/HW1_CB.jar
